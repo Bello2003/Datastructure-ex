56151ac666017671845524b2c2f417bf3c2a3f7d
Bello2003 <bellobilley93@gmail.com>
2026-01-05 23:17:15 +0300

    Implement a doubly linked list with insert and traverse

---
+++ ex5_doubly_linked_list.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new = Node(data)
+        new.next = self.head
+        if self.head:
+            self.head.prev = new
+        self.head = new
+
+    def traverse(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+
+dll = DoublyLinkedList()
+dll.insert(10)
+dll.insert(20)
+dll.traverse()
